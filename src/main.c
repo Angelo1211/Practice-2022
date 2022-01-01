336dd324718c96d447f38b48a98d1dee688c61ac
Angel Ortiz <angelo12@vt.edu>
2022-01-01 00:55:23 +0000

    Day3: Win32 Window
    Fixed the postquitmessage loop.
    
    Revert to starting point

--- src/main.c
+++ src/main.c
@@ -1,9 +1,18 @@
 #pragma comment(lib, "user32.lib")
 
 LRESULT CALLBACK
-WindowProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+WindowProc(HWND handle, UINT msg, WPARAM wp, LPARAM lp)
 {
-	return DefWindowProc(hwnd, msg, wp, lp);
+	switch(msg)
+	{
+		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+			return 0;
+		}break;
+	}
+
+	return DefWindowProc(handle, msg, wp, lp);
 }
 
 int main(int argc, char *argv[])
@@ -11,34 +20,31 @@ int main(int argc, char *argv[])
 	WNDCLASSEX wc = {0};
 	wc.cbSize = sizeof(wc);
 	wc.lpfnWndProc = WindowProc;
-	wc.hInstance = GetModuleHandle(0);
 	wc.lpszClassName = "Default Window Class";
+	wc.hInstance = GetModuleHandle(0);
 
 	RegisterClassEx(&wc);
 
 	HWND hwnd = CreateWindowEx(0, wc.lpszClassName,
-							   "Test Window",
-							   WS_OVERLAPPEDWINDOW,
-							   0, 0, 1280, 720,
-							   0, 0, wc.hInstance, 0);
+							   "Hello World!", WS_OVERLAPPEDWINDOW,
+							   100, 100, 1280, 720, 0, 0, wc.hInstance, 0);
 	assert(hwnd);
 
 	ShowWindow(hwnd, 1);
 
 	b32 running = true;
-	while (running)
+	while(running)
 	{
 		MSG msg;
 		while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
 		{
-			if (msg.message == WM_QUIT)
-			{
+			if(msg.message == WM_QUIT) 
 				running = false;
-				break;
-			}
 
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
 	}
+
+	printf("Hello World!\n");
 }
